8ca6e65a29bc37a35ff9f8bf9ba6f7da2190fc39
James Kirkby <jkirkby@protonmail.ch>
2017-07-25 21:55:43 +0800

    update thing entity

--- src/Entities/Thing.php
+++ src/Entities/Thing.php
@@ -1,48 +1,48 @@
 <?php
 
-namespace Jkirkby91\DoctrineSchemas\Entities;
+	namespace Jkirkby91\DoctrineSchemas\Entities;
 
-use Doctrine\ORM\Mapping as ORM;
-use Jkirkby91\LumenDoctrineComponent\Entities\LumenDoctrineEntity;
+	use Doctrine\ORM\Mapping as ORM;
+	use Jkirkby91\LumenDoctrineComponent\Entities\LumenDoctrineEntity;
 
-/**
- * Class Thing
- *
- * @package ApiArchitect\Entities
- * @author James Kirkby <jkirkby91@gmail.com>
- *
- * @ORM\MappedSuperclass
- */
-abstract class Thing extends LumenDoctrineEntity implements \Jkirkby91\Boilers\SchemaBoilers\SchemaContract
-{
-    /**
-     * @ORM\Column(type="string", length=299, nullable=false)
-     */
-    public $name;
+	/**
+	 * Class Thing
+	 *
+	 * @package ApiArchitect\Entities
+	 * @author James Kirkby <jkirkby91@gmail.com>
+	 *
+	 * @ORM\MappedSuperclass
+	 */
+	abstract class Thing extends LumenDoctrineEntity implements \Jkirkby91\Boilers\SchemaBoilers\SchemaContract
+	{
+		/**
+		 * @ORM\Column(type="string", length=299, nullable=false)
+		 */
+		public $name;
 
-    /**
-     * @param $name
-     */
-    public function __construct($name)
-    {
-        $this->setName($name);
-    }
+		/**
+		 * @param $name
+		 */
+		public function __construct($name)
+		{
+			$this->setName($name);
+		}
 
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
+		/**
+		 * @return mixed
+		 */
+		public function getName()
+		{
+			return $this->name;
+		}
 
-    /**
-     * @param $name
-     * @return $this
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-        return $this;
-    }
-}
+		/**
+		 * @param $name
+		 * @return $this
+		 */
+		public function setName($name)
+		{
+			$this->name = $name;
+			return $this;
+		}
+	}
