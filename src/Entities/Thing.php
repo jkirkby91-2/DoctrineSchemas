b946640d4d5741cfe782c6ac6cb7f61d161f7c2b
James Kirkby <jkirkby@protonmail.ch>
2017-10-20 16:49:30 +0200

    udpates

--- src/Entities/Thing.php
+++ src/Entities/Thing.php
@@ -1,48 +1,53 @@
 <?php
 
-	namespace Jkirkby91\DoctrineSchemas\Entities;
+	namespace Jkirkby91\DoctrineSchemas\Entities {
 
-	use Doctrine\ORM\Mapping as ORM;
-	use Jkirkby91\LumenDoctrineComponent\Entities\LumenDoctrineEntity;
-
-	/**
-	 * Class Thing
-	 *
-	 * @package ApiArchitect\Entities
-	 * @author James Kirkby <jkirkby91@gmail.com>
-	 *
-	 * @ORM\MappedSuperclass
-	 */
-	abstract class Thing extends LumenDoctrineEntity implements \Jkirkby91\Boilers\SchemaBoilers\SchemaContract
-	{
-		/**
-		 * @ORM\Column(type="string", length=299, nullable=false)
-		 */
-		public $name;
+		use Doctrine\ORM\Mapping as ORM;
+		use \Jkirkby91\Boilers\SchemaBoilers\SchemaContract;
+		use Jkirkby91\LumenDoctrineComponent\Entities\LumenDoctrineEntity;
 
 		/**
-		 * @param $name
+		 * Class Thing
+		 *
+		 * @package ApiArchitect\Entities
+		 * @author James Kirkby <jkirkby91@gmail.com>
+		 *
+		 * @ORM\MappedSuperclass
 		 */
-		public function __construct($name)
+		abstract class Thing extends LumenDoctrineEntity implements SchemaContract
 		{
-			$this->setName($name);
-		}
+			/**
+			 * @ORM\Column(type="string", length=299, nullable=false)
+			 */
+			public $name;
 
-		/**
-		 * @return mixed
-		 */
-		public function getName()
-		{
-			return $this->name;
-		}
+			/**
+			 * @param $name
+			 */
+			public function __Construct($name)
+			{
+				$this->setName($name);
+			}
 
-		/**
-		 * @param $name
-		 * @return $this
-		 */
-		public function setName($name)
-		{
-			$this->name = $name;
-			return $this;
+			/**
+			 * getName()
+			 * @return string
+			 */
+			public function getName() : string
+			{
+				return $this->name;
+			}
+
+			/**
+			 * setName()
+			 * @param $name
+			 *
+			 * @return \Jkirkby91\DoctrineSchemas\Entities\Thing
+			 */
+			public function setName($name) : Thing
+			{
+				$this->name = $name;
+				return $this;
+			}
 		}
 	}
